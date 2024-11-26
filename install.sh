9e5c3e107f98bbffc538e58021b9280236964e8e
Simone Karin Lehmann <skl@lisanet.de>
2024-11-27 00:34:03 +0100

    split single script into an installer script and the actual locactionchanger script. update to work with macOS 14+

---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/zsh
+
+INSTALL_DIR=/usr/local/bin
+SCRIPT_NAME=locationchanger
+LAUNCH_AGENTS_DIR=$HOME/Library/LaunchAgents
+PLIST_NAME=$LAUNCH_AGENTS_DIR/de.lisanet.LocationChanger.plist
+
+
+sudo mkdir -p "$INSTALL_DIR"
+sudo cp locationchanger "$INSTALL_DIR"
+sudo chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
+
+# generate a default config file if it doesn't exists
+APP_SUPPORT_DIR="$HOME/Library/Application Support/LocationChanger"
+if [ ! -e "$APP_SUPPORT_DIR/LocationChanger.conf" ]; then
+mkdir -p "$APP_SUPPORT_DIR"
+
+cat > "$APP_SUPPORT_DIR/LocationChanger.conf" << EOT
+[General]
+# specify the default Location to use. The default is 'Automatic'
+#DEFAULT_LOCATION=Automatic
+# To enable notifications, set to 1. For verbose notifications, set to 2. To disable, set to 0.
+#ENABLE_NOTIFICATIONS=1
+
+[Automatic]
+# [Automatic] defines a mapping for Wi-Fi Network SSIDs to Location names as key-value pairs.
+# Spaces are supported for both the SSID as well as the Location name, but all spaces
+# around the '=' will be trimmed. Additionally, do not enclose the SSID or Location in quotes
+# SSID=Location name
+
+[Manual]
+# This section contains a list of Location names for which autodetection and Location
+# switching should be ignored.
+# Wi-Fi Only
+
+EOT
+fi
+
+mkdir -p "$LAUNCH_AGENTS_DIR"
+
+cat > "$PLIST_NAME" << EOT
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>de.lisanet.locationchanger</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>$INSTALL_DIR/$SCRIPT_NAME</string>
+    </array>
+    <key>WatchPaths</key>
+    <array>
+        <string>/Library/Preferences/SystemConfiguration/com.apple.wifi.message-tracer.plist</string>
+    </array>
+</dict>
+</plist>
+EOT
+
+USERID=$SUDO_UID
+[ -z "$USERID" ] && USERID=$UID
+launchctl bootstrap gui/$USERID "$PLIST_NAME"
