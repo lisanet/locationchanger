4e76f14a070bbd41bd08a7f809158a04f70157a2
Simone Karin Lehmann <skl@lisanet.de>
2024-11-27 00:35:57 +0100

    use bootout ofr launchctl, prettify some code

--- uninstall.sh
+++ uninstall.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 INSTALL_DIR=/usr/local/bin
-SCRIPT_NAME=$INSTALL_DIR/locationchanger
+SCRIPT_NAME=locationchanger
 LAUNCH_AGENTS_DIR=$HOME/Library/LaunchAgents
 PLIST_NAME=$LAUNCH_AGENTS_DIR/LocationChanger.plist
 
 echo "This will uninstall LocationChanger and its config files and scripts from your Mac.\n"
-echo "Are you sure to uninstall LocationCahnger (y/n)?"
+echo "Are you sure to uninstall LocationChanger (y/n)?"
 read reply
 if [ "$reply" != "y" ]; then
     echo "Aborting uninstall command."
     exit
 fi
 
-sudo rm "$SCRIPT_NAME"
-launchctl unload "$PLIST_NAME"
+sudo rm "$INSTALL_DIR/$SCRIPT_NAME"
+
+USERID=$SUDO_UID
+[ -z "$USERID" ] && USERID=$UID
+launchctl bootout gui/$USERID "$PLIST_NAME"
+
 rm "$PLIST_NAME"
 rm -rf "$HOME/Library/Application Support/LocationChanger"
-rm -rf "${HOME}/Library/Logs/LocationChanger.log"
+rm -rf "$HOME/Library/Logs/LocationChanger.log"
 
 echo "Uninstall complete."
